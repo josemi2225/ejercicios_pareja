165e7222f75560a20612b05ec3c12e6257c4a05d
JosueEsau <00220425@uca.edu.sv>
2025-05-06 18:01:57 -0600

    Add files via upload

---
+++ usuario_contra.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main(void){
+   
+    int usuario , contra;
+    cout<<"Ingrese su usuario:\n";
+    cin>>usuario;
+    if(usuario == 1234){
+        cout<<"Ingrese su contra\n";
+        cin>>contra;
+        if(contra == 1234){
+            cout<<"contra correcta\n"; 
+        }else{
+            cout<<"contra incorrecta\n";
+        }
+
+    }else{
+        cout<<"Usuario no encontrado\n";
+        return 0;
+    }
+
+}
